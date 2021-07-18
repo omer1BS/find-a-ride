e7148bfecfd5a1477597270af186b6cd5c894e51
omer1BS <omer1.bs@gmail.com>
2021-07-18 16:22:16 +0300

    added fields in user

---
+++ server/models/ride.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    firstName: {type: String, required: true},
+    lastName: {type: String, required:true },
+    dateOfBirth: {type: Date, required:true},
+    gender:{type: String, required:true, enum:['Male','Female','Other']},
+    password: {type: String, required: true},
+})
+
+module.exports = mongoose.model('User', userSchema)
